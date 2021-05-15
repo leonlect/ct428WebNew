344de29584028162b9d55adee6d8dc0032729ead
leonle <codebreakerct@gmail.com>
2021-05-15 23:50:25 +0700

    Fix file xuly

--- input_xuly.php
+++ input_xuly.php
@@ -5,19 +5,25 @@ $ten = $_POST['fname'];
 $ho = $_POST['lname'];
 $tuoi = $_POST['age'];
 //Lay ve path hinh anh
-$path;
+$path = $_FILES['avatar']['name'];
 
 
-echo "Your first name is: " . $ten .  ". Last name is : " . $ho . ". Age: " . $tuoi;
+
+echo "Your first name is: " . $ten . "<br/>" .
+     "Last name is : " . $ho . "<br/>" .
+     "Age: " . $tuoi . "<br/>";
 
 // Buoc 1Tao doi tuong ket noi voi 4 tham so
 $con = new mysqli("localhost", "root", "", "thanhvien");
 $con->set_charset("utf8");
 //Buoc 2 Tao cau truy van
-$sql = " INSERT INTO info(firstname, lastname, age) VALUES ('$ten', '$ho', $tuoi)  ";
+$sql = " INSERT INTO info(firstname, lastname, age, avatar)       VALUES ('$ten', '$ho', $tuoi, '$path')  ";
 //In ra man hinh thu cau truy van
 echo $sql;
 //Buoc 3 thuc hien cau truy van
 $con->query($sql);
+move_uploaded_file($_FILES['avatar']['tmp_name'], $path); //Upload file vao thu muc can chua hinh anh
+echo "<br/><h2>Insert Success !</h2>";
 //Dong ket noi
 $con->close();
+?>
