b2878d989a5723ee1f5b2c52cbaeac8a16209dd4
leonle <codebreakerct@gmail.com>
2021-05-17 23:05:59 +0700

    Add logout.php | Done !

---
+++ buoi3/dangxuat.php
@@ -0,0 +1,6 @@
+<?php
+ session_start();
+ session_destroy();
+ echo "<h2>Bạn đã đăng xuất khỏi hệ thống. Xin cám ơn !</h2>";
+ echo "<p>Quay về trang đăng nhập <a href='dangnhap.html'>Click vào đây</a> !</p>";
+ ?>
